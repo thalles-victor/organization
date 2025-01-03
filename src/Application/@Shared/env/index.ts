b844693f658a2163b9489f7315df7708b6a20195
thalles-victor <thalles.victor2012@gmail.com>
2025-01-03 17:08:16 -0300

    :wrench: conf: create postgres and redis, conf the env properties.

--- src/Application/@Shared/env/index.ts
+++ src/Application/@Shared/env/index.ts
@@ -1,7 +1,29 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
+  // JWT
   JWT_SECRET: z.string(),
+
+  // POSTGRES
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  POSTGRES_PORT: z
+    .string()
+    .refine((val) => !isNaN(parseInt(val, 10)), {
+      message: 'POSTGRES_PORT must be a valid number',
+    })
+    .transform((val) => parseInt(val, 10)),
+
+  // REDIS
+  REDIS_HOST: z.string(),
+  REDIS_PORT: z
+    .string()
+    .refine((val) => !isNaN(parseInt(val, 10)), {
+      message: 'POSTGRES_PORT must be a valid number',
+    })
+    .transform((val) => parseInt(val, 10)),
+  REDIS_PASSWORD: z.string(),
 });
 
 export const ENV = envSchema.parse(process.env);
