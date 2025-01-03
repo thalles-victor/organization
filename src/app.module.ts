d0d28cf0e0da67b109c85828c4159616901c7a3a
thalles-victor <thalles.victor2012@gmail.com>
2025-01-03 16:54:32 -0300

    :wrench: conf: enable class-validator and class-transform.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        transform: true, // Transforma os parâmetros automaticamente
+        whitelist: true, // Remove campos não validados
+      }),
+    },
+
+    AppService,
+  ],
 })
 export class AppModule {}
