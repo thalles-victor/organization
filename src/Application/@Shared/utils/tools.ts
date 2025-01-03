99192faf5abf071d0e202957a8ad1814ea8ec1e2
thalles-victor <thalles.victor2012@gmail.com>
2025-01-03 16:55:04 -0300

    :sparkles: feat: create pagination dto with validations.

--- src/Application/@Shared/utils/tools.ts
+++ src/Application/@Shared/utils/tools.ts
@@ -1,4 +1,40 @@
+import { Transform } from 'class-transformer';
+import {
+  IsOptional,
+  IsInt,
+  Min,
+  IsString,
+  IsObject,
+  IsIn,
+} from 'class-validator';
 import { InternalServerErrorException } from '@nestjs/common';
+
+export class GenericPaginationDto {
+  @IsOptional()
+  @IsInt()
+  @Min(1, { message: 'The page must be at least 1' })
+  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+  page?: number = 1;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1, { message: 'The limit must be at least 1.' })
+  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  search?: string;
+
+  @IsOptional()
+  @IsObject()
+  filters?: Record<string, any>;
+
+  @IsString()
+  @IsOptional()
+  @IsIn(['ASC', 'DESC'], { message: 'Order must be ASC or DESC' })
+  order?: 'ASC' | 'DESC' = 'DESC';
+}
 
 export function splitKeyAndValue(param: object) {
   const [key, value] = Object.entries(param)[0];
