56cbce54520a0a71d13f958810551d94dcc0858e
thalles-victor <thalles.victor2012@gmail.com>
2025-01-03 23:57:36 -0300

    :test_tube: test: SignUpUseCase.

---
+++ src/Application/Domains/Auth/UseCases/SignUp/SignUp.usecase.spec.ts
@@ -0,0 +1,56 @@
+/* eslint-disable prettier/prettier */
+import { Test, TestingModule } from '@nestjs/testing';
+import { SignUpUseCase } from './SignUp.usecase';
+import { SignUpDto } from './SignUp.dto';
+import { KEY_OF_INJECTION } from '#metadata';
+import { UserInMemoryRepository } from 'src/Application/Infra/Repositories/User/UserInMemory.repository';
+import { ROLE } from 'src/Application/@Shared/metadata/role';
+
+describe('UserService', () => {
+  let signUpUseCase: SignUpUseCase;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SignUpUseCase,
+        {
+          provide: KEY_OF_INJECTION.USER_REPOSITORY,
+          useClass: UserInMemoryRepository,
+        },
+      ],
+    }).compile();
+
+    signUpUseCase = module.get<SignUpUseCase>(SignUpUseCase);
+  });
+
+  describe('Create user Account', () => {
+    it('seja possivel criar um usuário que não exista', async () => {
+      const userDto: SignUpDto = {
+        name: 'thalles',
+        email: 'thalles@gmail.com',
+        password: 'minhasenha123',
+      };
+
+      const userCreated = await signUpUseCase.execute(userDto);
+
+      expect(userCreated.name).toBe(userDto.name);
+      expect(userCreated.password).not.toBe(userDto.password);
+      expect(userCreated.role).toBe(ROLE.USER);
+      expect(userCreated.role).toBeDefined();
+    });
+
+    it('não seja possível criar um usuário que já exista', async () => {
+      const userDto: SignUpDto = {
+        name: 'thalles',
+        email: 'thalles@gmail.com',
+        password: 'minhasenha123',
+      };
+
+      await signUpUseCase.execute(userDto);
+
+      expect(
+        async () => await signUpUseCase.execute(userDto),
+      ).rejects.toThrow();
+    });
+  });
+});
