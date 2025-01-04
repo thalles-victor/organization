3eed73ace109f4d008765aedb2f3c3646ef6a033
thalles-victor <thalles.victor2012@gmail.com>
2025-01-03 23:57:14 -0300

    :wrench: conf: import alias in jest.

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+};
+
+export default config;
