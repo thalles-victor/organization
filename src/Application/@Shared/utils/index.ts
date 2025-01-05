bebb8a42eff91fd51579945c0d3b821958206f66
thalles-victor <thalles.victor2012@gmail.com>
2025-01-05 08:46:05 -0300

    :sparkles: feat: create functions to generate and check access tokens.

--- src/Application/@Shared/utils/index.ts
+++ src/Application/@Shared/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './tools';
 export * from './id';
+export * from './jwt-token';
---
+++ src/Application/@Shared/utils/jwt-token.ts
@@ -0,0 +1,42 @@
+import * as jwt from 'jsonwebtoken';
+import { ENV } from '../env';
+import { UnauthorizedException } from '@nestjs/common';
+import { PayloadType } from '../types';
+
+export async function genJWTAccess(payload: object) {
+  const duration = '24h';
+  const expiresInMilliseconds = 24 * 60 * 60 * 1000; // 86400000 mile seconds
+  const expiresInUnixEpoch = Date.now() + expiresInMilliseconds;
+
+  const token = jwt.sign(payload, ENV.JWT_SECRET, {
+    expiresIn: '24h',
+    algorithm: 'ES512',
+  });
+
+  return {
+    token,
+    duration,
+    expiresInMilliseconds,
+    expiresInUnixEpoch,
+  };
+}
+
+export async function verifyJWTAccess(jwtAccess: string): Promise<PayloadType> {
+  try {
+    const payload = jwt.verify(jwtAccess, ENV.JWT_SECRET);
+
+    return payload as PayloadType;
+  } catch {
+    throw new UnauthorizedException();
+  }
+}
+
+export async function jwtAlreadyExpireBaseUnixEpoch(
+  expiresInUnixEpoch: number,
+) {
+  if (Date.now() > expiresInUnixEpoch) {
+    return true;
+  }
+
+  return false;
+}
